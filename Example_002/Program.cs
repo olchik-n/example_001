46c5f1a2b42a347a6417457f85e4b50fe1bbdbcf
Olga <olchik-n@mail.ru>
2022-11-20 23:54:23 +0300

    add csproj files

--- Example_002/Program.cs
+++ Example_002/Program.cs
@@ -1,4 +1,2 @@
-﻿Console.Write ("Введите ваше имя");
-string username = Console.ReadLine();
-Console.Write("Привет, ");
-Console.Write(username);
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
